87070528da17b571df62f92091599a5b0ef52846
deriolucas <dee_lukas@hotmail.com>
2020-11-24 22:08:42 -0300

    button

--- pages/index.js
+++ pages/index.js
@@ -4,11 +4,15 @@ import styles from '../styles/Home.module.css'
 export default function Home() {
   return (
     <div className={styles.container}>
-      <Head>
-        <title>Projetos de IoT</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <h1>Monitoramento do Cultivo</h1>
+    <Head>
+      <title>Projetos de IoT</title>
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
+      <div className={styles.header}>
+        <h1>Monitoramento do Cultivo</h1>
+        <input type="checkbox" hidden="hidden" id="username" />
+        <label className={styles.switch} for="username"></label>
+      </div>
       <div className={styles.tabelas}>
         <div className={styles.tabela}>
           <h2>Umidade</h2>
