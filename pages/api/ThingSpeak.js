e02eb439bd52691c4298801e1a1d6640cb2fbd9c
Deustavo <gustavo.andrade00@outlook.com>
2020-11-25 23:04:30 -0300

    apago KKKKKKKKKKKKKKKKKKKK

--- pages/api/ThingSpeak.js
+++
@@ -1,7 +0,0 @@
-export function ThingSpeak() {
-    fetch('https://api.thingspeak.com/channels/1228925/fields/1.json?Results=2').then((res) => {
-        res.json().then((res) => {
-            return res 
-            });
-    });
-}
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import Head from 'next/head';
-
-import { ThingSpeak } from './api/ThingSpeak';
 import styles from '../styles/Home.module.css';
 
 export default function Home() {
